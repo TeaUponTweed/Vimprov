dc01975b737276db1ff48a36700afa2b8b2f4243
Michael Mason <TeaUponTweed@gmail.com>
2019-10-27 15:39:42 -0600

    make code more generic

--- Vimprov.py
+++ Vimprov.py
@@ -2,16 +2,17 @@ import sublime
 import sublime_plugin
 
 
-def _set_color_scheme(scheme, settings=None):
+def _get_path_to_scheme(scheme):
     color_schemes = sublime.find_resources("*.sublime-color-scheme")
     for color_scheme in color_schemes:
         if scheme in color_scheme:
-            print(color_scheme)
-            _do_set_color_scheme_tmp(color_scheme, settings=settings)
-            break
+            return color_scheme
     else:
         raise ValueError('No scheme matching {} found'.format(scheme))
 
+def _set_color_scheme(scheme, settings=None):
+    scheme = _get_path_to_scheme(scheme)
+    _do_set_color_scheme_tmp(scheme, settings=settings)
 
 def _do_set_color_scheme_tmp(color_scheme_path, settings):
     if settings is None:
@@ -25,23 +26,45 @@ def _load_settings():
 
 class EnterVimprovCommand(sublime_plugin.WindowCommand):
     def run(self):
-        _set_color_scheme('Stark')
+        pass
+        # _set_color_scheme('Stark')
 
 
 class ExitVimprovCommand(sublime_plugin.WindowCommand):
     def run(self):
-        _set_color_scheme('Monokai')
+        pass
+        # _set_color_scheme('Monokai')
+
 
 
 def do_toggle_vimprov(view):
     settings = view.settings()
     vimprov = not settings.get('vimprov', False)
     settings.set('vimprov', vimprov)
-    print(sublime.find_resources("*.sublime-color-scheme"))
     if vimprov:
-        _set_color_scheme('Stark', settings)
+        stark_color_theme_loc = settings.get('vimprov_stark_them')
+        if stark_color_theme_loc is None:
+            stark_color_theme_loc = _get_path_to_scheme('Stark')
+            settings.set('vimprov_stark_them', stark_color_theme_loc)
+
+        prev_theme = settings.get('color_scheme')
+
+        print(prev_theme, '~>', stark_color_theme_loc)
+
+        settings.set('vimprov_prev_theme', prev_theme)
+
+        _do_set_color_scheme_tmp(stark_color_theme_loc, settings)
+
     else:
-        _set_color_scheme('Monokai', settings)
+        stark_color_theme_loc = settings.get('vimprov_stark_them')
+        assert stark_color_theme_loc is not None
+
+        prev_theme = settings.get('vimprov_prev_theme')
+        assert prev_theme is not None
+
+        print(stark_color_theme_loc, '~>', prev_theme)
+
+        _do_set_color_scheme_tmp(prev_theme, settings)
 
 
 class ToggleVimprovCommand(sublime_plugin.WindowCommand):
