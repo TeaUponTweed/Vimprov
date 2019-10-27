e9fd768a1e2bcb59c7fe4e1477a6d6af5e87ea16
Michael Mason <TeaUponTweed@gmail.com>
2019-10-27 15:26:46 -0600

    add toggle

--- Vimprov.py
+++ Vimprov.py
@@ -1,28 +1,56 @@
 import sublime
 import sublime_plugin
 
-def _set_color_scheme(scheme):
+
+def _set_color_scheme(scheme, settings=None):
     color_schemes = sublime.find_resources("*.sublime-color-scheme")
     for color_scheme in color_schemes:
         if scheme in color_scheme:
-            _do_set_color_scheme(color_scheme)
+            print(color_scheme)
+            _do_set_color_scheme_tmp(color_scheme, settings=settings)
             break
     else:
         raise ValueError('No scheme matching {} found'.format(scheme))
 
-def _do_set_color_scheme(color_scheme_path):
-    _load_settings().set('color_scheme', color_scheme_path)
-    sublime.save_settings('Preferences.sublime-settings')
-    sublime.status_message('SelectColorScheme: ' + color_scheme_path)
+
+def _do_set_color_scheme_tmp(color_scheme_path, settings):
+    if settings is None:
+        settings = _load_settings()
+    settings.set('color_scheme', color_scheme_path)
+    # sublime.save_settings('Preferences.sublime-settings')
+    # sublime.status_message('SelectColorScheme: ' + color_scheme_path)
+
 
 def _load_settings():
     return sublime.load_settings('Preferences.sublime-settings')
 
 
-
 class EnterVimprovCommand(sublime_plugin.WindowCommand):
     def run(self):
         _set_color_scheme('Stark')
+
+
 class ExitVimprovCommand(sublime_plugin.WindowCommand):
     def run(self):
         _set_color_scheme('Monokai')
+
+
+def do_toggle_vimprov(view):
+    settings = view.settings()
+    vimprov = not settings.get('vimprov', False)
+    settings.set('vimprov', vimprov)
+    print(sublime.find_resources("*.sublime-color-scheme"))
+    if vimprov:
+        _set_color_scheme('Stark', settings)
+    else:
+        _set_color_scheme('Monokai', settings)
+
+
+class ToggleVimprovCommand(sublime_plugin.WindowCommand):
+    def run(self):
+        # print(sublime.settings())
+        for w in sublime.windows():
+                for v in w.views():
+                    do_toggle_vimprov(v)
+                    # print()
+                    # v.settings().set('vimprov', not v.settings().set('vimprov')
